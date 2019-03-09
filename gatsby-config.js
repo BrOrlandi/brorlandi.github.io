832c01ab976f0fbce8d3b2210ec59014b90711f8
BrOrlandi <brorlandi@gmail.com>
2019-03-09 02:19:15 -0300

    Config Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,34 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Bruno Orlandi',
+    description: 'Site pessoal de Bruno Orlandi.  Empreendedor e Desenvolvedor Front-end. Compartilhando conhecimento, projetos e ideias sobre tecnologia, empreendedorismo e desenvolvimento pessoal.',
+    author: '@brorlandi',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'Bruno Orlandi',
+        short_name: 'Bruno Orlandi',
+        start_url: '/',
+        background_color: '#0057a1',
+        theme_color: '#0057a1',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
